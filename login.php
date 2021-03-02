ffc77c4d821542fd00030e82f2702e4aebce1ef3
chuonglehuu <66240698+chuonglehuu@users.noreply.github.com>
2021-03-02 13:53:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,9 +3,9 @@ session_start();
 $username = $_POST['Username'];
 $password = $_POST['Password'];
     if ($username == 'admin' && $password == '123') {
-        header("location:table.html");
+        header("location:Add.html");
     } elseif ($username == 'boss' && $password == '123') {
-        header("location:dashboard.html");
+        header("location:tables.html");
     } else
     {
         header("location:login.html");
